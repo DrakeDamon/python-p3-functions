0610e2caa340986e51aa242c982940e3837c9057
Drake Damon <dddamon06@gmail.com>
2024-10-10 10:58:39 -0400

    completed functions lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(name="programmer"):
+    print(f"Hello, {name}!")
 
-def greet(name):
-    pass
 
+greet_programmer()
+
+def greet(name= "Guido"):
+    print(f"Hello, {name}!")
+
+greet()
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 +  num2
+add(45, 55)
 
-def halve(number):
-    pass
+def halve(value):
+    return value / 2
+halve(100)
